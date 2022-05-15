e1457510033432baaff8334fc3684cd0a9b7c304
Moringa <alinoorshukri8@gmail.com>
2022-05-15 20:49:47 +0300

    added a Questions array which i will later use as a my database

---
+++ Js/Quiz.js
@@ -0,0 +1,52 @@
+const QuizData =[
+    {
+        quiz: 'Q1:Which language runs in web brwoser',
+        a:'java',
+        b:'C',
+        c:'python',
+        d:'Java Script',
+
+        correct: 'd'
+   
+    },
+    {
+        quiz: 'Q2: JS Frameworks tought in Moringa school for mpp25',
+        a:'java',
+        b:'C',
+        c:'React',
+        d:'Java Script',
+
+        correct: 'c'
+   
+    },
+    {
+        quiz: 'Q3: CSS Stands for',
+        a:'Cascading Style Sheet',
+        b:'Computer science society',
+        c:'scripting language',
+        d:'cascading styles script',
+
+        correct: 'a'
+   
+    },
+    {
+        quiz: 'Q4:Which one is not HTML Element',
+        a:'padding',
+        b:'h2',
+        c:'p',
+        d:'div',
+
+        correct: 'a'
+   
+    },
+    {
+        quiz: 'Q4:Which type of element is typically a block?',
+        a:'<p>',
+        b:'<q>',
+        c:'<i>',
+        d:'<b>',
+
+        correct: 'a'
+   
+    }
+];
