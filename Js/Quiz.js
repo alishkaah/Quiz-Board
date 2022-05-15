29bee1947ab0fdf4ebabd879820b3dd3fa69d948
Moringa <alinoorshukri8@gmail.com>
2022-05-15 21:13:39 +0300

    had an error when i placed a return statement inside loop, Made changes to return anwer outside loop

--- Js/Quiz.js
+++ Js/Quiz.js
@@ -90,13 +90,13 @@ function deselectAnswers(){
 }
 
 function getSelected(){
+
     let answer;
     answers.forEach(currentAns=>{
-
         if(currentAns.checked){
             answer= currentAns.id;
         }  
-        return answer
+        
 })
-
+return answer
 }
